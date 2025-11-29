699049df325da118ba5ae1bc24af68a0567cb2c3
Mohamed Eyad <mooeyad@gmail.com>
2025-11-29 17:31:01 +0100

    Initialize aruco_visual_servoing & aruco_interfaces

---
+++ aruco_visual_servoing/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'aruco_visual_servoing'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Mohamed Eyad',
+    maintainer_email='mooeyad@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
